243b0045473abea9f9c900268bb2323b1438d37a
prustysitikantha <sitikanthaprusty2904@gmail.com>
2026-06-17 01:51:47 +0530

    FIX STUN

--- main.py
+++ main.py
@@ -204,7 +204,9 @@ def main():
             video_processor_factory=VideoProcessorClass,
             rtc_configuration={
                 "iceServers": [
-                    {"urls": ["stun:stun.l.google.com:19302"]}
+                    {"urls": ["stun:stun.l.google.com:19302"]},
+                    {"urls": ["stun:stun1.l.google.com:19302"]},
+                    {"urls": ["stun:stun2.l.google.com:19302"]},
                 ]
             },
             media_stream_constraints={
